69d66a3843179bc61d79e829d77d7c8f54199400
Rafael Cortes <rafael_cortes@outlook.com>
2024-06-10 15:42:34 -0300

    Estruturas Condicionais

---
+++ src/br/com/rcdeo/Conditional.java
@@ -0,0 +1,36 @@
+package br.com.rcdeo;
+
+public class Conditional {
+
+    public static void main(String[] args) {
+
+        int nota = 100;
+        String graducao;
+
+        if (nota >= 80) {
+            graducao = "A";
+        } else if (nota < 80 && nota >= 70) {
+            graducao = "B";
+        } else if (nota < 70 && nota >= 60) {
+            graducao = "C";
+        } else if (nota < 60 && nota >= 0) {
+            graducao = "D";
+        } else {
+            graducao = "";
+        }
+
+        switch (graducao) {
+            case "A":
+            case "B":
+                System.out.println("Aluno aprovado!");
+                break;
+            case "C":
+            case "D":
+                System.out.println("Aluno reprovado.");
+                break;
+            default:
+                System.out.println("Graduação inválida");
+        }
+
+    }
+}
