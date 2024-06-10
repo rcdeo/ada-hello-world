c6f6f48e1e8c4fd6e8f4afa42305edbd8194170d
Rafael Cortes <rafael_cortes@outlook.com>
2024-06-10 14:01:36 -0300

    Variáveis

--- src/br/com/rcdeo/Main.java
+++ src/br/com/rcdeo/Main.java
@@ -4,7 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        int a;
+        int b = 2;
+        a = 3;
+        int   soma          = a + b;
+        int   subtracao     = a - b;
+        int   multiplicacao = a * b;
+        float divisao       = (float) a / b;
+        int   resto         = a % b;
 
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicacao);
+        System.out.println(divisao);
+        System.out.println(resto);
     }
 }
