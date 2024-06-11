55cd2f219d110781748cb80099d733356379b28b
Rafael Cortes <rafael_cortes@outlook.com>
2024-06-10 22:22:28 -0300

    Funções

---
+++ src/br/com/rcdeo/Functions.java
@@ -0,0 +1,22 @@
+package br.com.rcdeo;
+
+public class Functions {
+
+    public static void main(String[] args) {
+
+//        String nomeOriginal = "Beltrano";
+//        saudacao(nomeOriginal);
+
+        int resultado = soma(2, 3);
+        System.out.println(resultado);
+
+    }
+
+//    public static void saudacao(String nomeParametro) {
+//        System.out.println("Olá, " + nomeParametro + ".");
+//    }
+
+    public static int soma(int a, int b) {
+        return a + b;
+    }
+}
